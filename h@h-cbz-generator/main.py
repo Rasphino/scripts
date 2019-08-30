be58b2739baace8c92ca0511654d62cc56568373
Honghao Li <hh.li99@outlook.com>
2019-08-30 23:40:33 +0800

    [h@h]: init

---
+++ h@h-cbz-generator/main.py
@@ -0,0 +1,18 @@
+import shutil
+import os
+
+if __name__ == '__main__':
+    # download_path = '/home/hath/hath/download'
+    download_path = './test'
+    try:
+        os.chdir(download_path)
+        subfolders = [f.name for f in os.scandir('.') if f.is_dir()]
+    except Exception as e:
+        print("Failed to find subdirectories in {}".format(download_path))
+
+    for subfolder in subfolders:
+        if not os.path.exists('./{}/galleryinfo.txt'.format(subfolder)):
+            print('Gallery {} is not complete!'.format(subfolder))
+            continue
+        shutil.make_archive(subfolder, 'zip', root_dir='./{}'.format(subfolder))
+        os.rename('{}.zip'.format(subfolder), '{}.cbz'.format(subfolder))
